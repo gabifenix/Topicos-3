ed299a4b0c4deca2ed078bdbbd55f74108521c30
Gabriel Danilo Benetti <g.d.benetti@hotmail.com>
2021-06-01 22:15:54 -0300

    Projeto Vacina 01-06 Atualizado

--- vacinas/cadastro/views.py
+++ vacinas/cadastro/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .. cadastro.models import cadastroModel
+from datetime import date
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def cadastro(request):
+    verifica_feriado = False
+    nome_feriado = ''
+    hoje = date.today()
+    feriados = cadastroModel.objects.all()
+    for feriado in feriados:
+        if feriado.dia == hoje.day:
+            if feriado.mes == hoje.month:
+                verifica_feriado = True
+                nome_feriado = feriado.nome
+    contexto = {'feriado':verifica_feriado,
+                'nome_feriado':nome_feriado}
+    return render(request, 'mostra_feriado.html', contexto)
