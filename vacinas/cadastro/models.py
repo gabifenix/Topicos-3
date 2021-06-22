bc1f092ce7f26e8f97b81793d3d094efdd4d5f1e
Gabriel Danilo Benetti <g.d.benetti@hotmail.com>
2021-06-22 19:53:24 -0300

    Projeto Vacina 22-06

--- vacinas/cadastro/models.py
+++ vacinas/cadastro/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Cadastro(models.Model):
     nome = models.CharField('Nome',max_length=255)
-    idade = models.CharField('Idade',max_length=255)
-    data = models.CharField('Data',max_length=255)
-    numero_da_dose = models.CharField('Numero da Dose',max_length=255)
+    idade = models.CharField('Idade', max_length=255)
+    data = models.CharField('Data', max_length=255)
+    numero_da_dose = models.CharField('Numero da Dose', max_length=255)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
